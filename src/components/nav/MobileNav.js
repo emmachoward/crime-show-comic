9552d55fb5004fa68461152b05be326a53acae8d
emmachoward <46038545+emmachoward@users.noreply.github.com>
2021-10-13 18:16:54 -0500

    Started working on Next/Prev Buttons

--- src/components/nav/MobileNav.js
+++ src/components/nav/MobileNav.js
@@ -3,11 +3,16 @@ import React, { useState } from 'react';
 import classes from './MobileNav.module.css'
 import Search from './search/Search';
 import Sidebar from './sidebar/Sidebar';
+import { HashLink } from 'react-router-hash-link';
 
 const MobileNav = props => {
     const { comicData } = props;
     const [displaySidebar, setDisplaySidebar] = useState(false);
     const [displaySearch, setDisplaySearch] = useState(false);
+    const [currentComic, setCurrentComic] = useState(comicData[0]);
+    console.log(comicData[0]);
+    console.log(comicData.title);
+    //console.log(comicData[2].title);
 
     const displaySidebarHandler = () => {
         setDisplaySidebar(!displaySidebar);
@@ -15,6 +20,12 @@ const MobileNav = props => {
 
     const displaySearchHandler = () => {
         setDisplaySearch(!displaySearch);
+    };
+
+    const nextHandler = () => {
+        console.log(currentComic);
+        setCurrentComic(comicData[1].title)
+        return currentComic.title;
     };
 
     return (
@@ -27,8 +38,13 @@ const MobileNav = props => {
                             <img src="http://satyr.io/50x50/black" alt="" /><br />Menu
                         </button>
                     </li>
-                    <li><a href="#Prev"><img src="http://satyr.io/50x50/black" alt="" /><br />Prev</a></li>
-                    <li><a href="#Next"><img src="http://satyr.io/50x50/black" alt="" /><br />Next</a></li>
+                    {/* How do you know where on the page the user currently is? */}
+                    <li><HashLink to="/"><img src="http://satyr.io/50x50/black" alt="Previous" /><br />Prev</HashLink></li>
+                    <li><HashLink to={`/comics#${nextHandler}`}><img src="http://satyr.io/50x50/black" alt="Next" /><br />Next</HashLink></li>
+                    <li><button onClick={nextHandler} >
+                            <img src="http://satyr.io/50x50/black" alt="" /><br />Next
+                        </button>
+                    </li>
                     <li><button onClick={displaySearchHandler} >
                             <img src="http://satyr.io/50x50/black" alt="" /><br />Search
                         </button>
