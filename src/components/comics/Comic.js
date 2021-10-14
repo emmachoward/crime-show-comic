66c3e93d801095bfe860c076c3898661bd22ea5d
emmachoward <46038545+emmachoward@users.noreply.github.com>
2021-10-14 13:52:50 -0500

    Default image appears if no matching image

--- src/components/comics/Comic.js
+++ src/components/comics/Comic.js
@@ -4,13 +4,19 @@ import classes from './Comic.module.css'
 
 const Comic = props => {
     const { comicData } = props;
+    
     return (
         <>
             {comicData.map(comic =>
                 <div className={classes.comic} key={comic.id}  id={comic.link}>
                     <h2>{comic.title}</h2>
                     <p>{comic.date}</p>
-                    <img src="https://picsum.photos/500/2000" alt={comic.title} />
+
+                    <img src={`images/comics/${comic.image}`} 
+                    onError={(e)=>{e.target.onerror = null; 
+                    e.target.src="https://picsum.photos/500/2000"}} 
+                    alt={comic.title} /> 
+                    {/* Uses filler image when there's no matching image in the folder */}
                 </div>
             )}
             
